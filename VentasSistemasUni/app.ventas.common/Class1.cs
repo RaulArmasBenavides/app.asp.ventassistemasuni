669df781669d45e270ec2ab03329a9521d062f2d
Raúl Armas <raularmasbx@gmail.com>
2022-05-04 02:38:13 -0500

    update

---
+++ VentasSistemasUni/app.ventas.common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.ventas.common
+{
+    public class Class1
+    {
+    }
+}
